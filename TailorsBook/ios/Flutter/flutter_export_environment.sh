6b44f013a199bc6cdae1de9e0ff60a9f19ecdde9
Mukesh Choudhary <47542244+Mukesh2514@users.noreply.github.com>
2020-12-31 13:44:24 +0530

    my first commit

---
+++ TailorsBook/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/alpha/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/alpha/Desktop/Alpha/Tailor/TailorBook/TailorsBook"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/home/alpha/Development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
